b48965da2e6262a9469b35eafcf46239e4831b5f
Alan Young <harleypig@harleypig.com>
2024-02-11 08:55:08 -0700

    Update output value in linode_domain module to use domain_index instead of domain for Linode domain IDs.

--- linode_domain/outputs.tf
+++ linode_domain/outputs.tf
@@ -1,4 +1,4 @@
 output "domain_ids" {
-  value = { for d in linode_domain.domains : d.domain => d.id }
+  value = { for domain, details in linode_domain.domains : details.domain_index => d.id }
   description = "A map of domain names to their respective Linode domain IDs."
 }
