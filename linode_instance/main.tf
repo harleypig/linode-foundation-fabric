1734f0563e1f1823e14068dec6adda0f68651bd7
Alan Young <harleypig@harleypig.com>
2024-04-07 19:55:04 -0600

    aider: Replace linode_id = 2299 with the proper call to the generated id from the instance resource.
    # Aider chat conversation:
    
    USER: replace linode_id = 2299 with the proper call to the generated id from the instance resource.
    
    ASSISTANT: ```diff
    --- tfmods/linode_instance/main.tf
    +++ tfmods/linode_instance/main.tf
    @@ ... @@
     resource "linode_instance_disk" "site_disk_swap" {
    -  linode_id  = 2299
    +  linode_id  = linode_instance.site.id
       size       = 512
     }
    
    @@ ... @@
     resource "linode_instance_disk" "site_disk" {
    -  linode_id  = 2299
    +  linode_id  = linode_instance.site.id
       size       = 163328
     }
    
    @@ ... @@
     resource "linode_instance_config" "site_config" {
    -  linode_id    = 2299
    +  linode_id    = linode_instance.site.id
       memory_limit = 0
       root_device  = "/dev/sda"
       run_level    = "default"
    ```

--- linode_instance/main.tf
+++ linode_instance/main.tf
@@ -1,14 +1,14 @@
 resource "linode_instance_disk" "site_disk_swap" {
   filesystem = "swap"
   label      = "512 MB Swap Image"
-  linode_id  = 2299
+  linode_id  = linode_instance.site.id
   size       = 512
 }
 
 resource "linode_instance_disk" "site_disk" {
   filesystem = "ext4"
   label      = "Arch Linux Disk"
-  linode_id  = 2299
+  linode_id  = linode_instance.site.id
   size       = 163328
 }
 
@@ -17,7 +17,7 @@ resource "linode_instance_config" "site_config" {
   comments     = null
   kernel       = "linode/grub2"
   label        = "My Arch Linux Profile"
-  linode_id    = 2299
+  linode_id    = linode_instance.site.id
   memory_limit = 0
   root_device  = "/dev/sda"
   run_level    = "default"
