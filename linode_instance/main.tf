55f9f71cb6560b40b5cd26b700b16264d4af64f0
Alan Young <harleypig@harleypig.com>
2025-02-03 18:12:44 -0700

    chore: Clean up unused Linode instance configuration and comments

--- linode_instance/main.tf
+++ linode_instance/main.tf
@@ -1,3 +1,4 @@
+# Make this resource use the appropriate variables for each options, AI!
 resource "linode_instance" "site" {
   label = var.label
   #image  = "linode/arch"
@@ -5,77 +6,7 @@ resource "linode_instance" "site" {
   type   = var.type
   #migration_type = "cold"
   #resize_disk    = false
-
   backups_enabled = var.backups
-
-  # XXX: Can I control this via terraform?
-  #  backups = [
-  #    {
-  #      available = true
-  #      enabled   = true
-  #      schedule = [
-  #        {
-  #          day    = "Sunday"
-  #          window = "W10"
-  #        },
-  #      ]
-  #    },
-  #  ]
-
-  #alert {
-  #  cpu            = var.cpu_alert_threshold
-  #  io             = var.io_alert_threshold
-  #  network_in     = var.network_in_alert_threshold
-  #  network_out    = var.network_out_alert_threshold
-  #  transfer_quota = var.transfer_quota_alert_threshold
-  #}
   alert = var.alert
-
   tags = var.tags
 }
-
-#resource "linode_instance_config" "site_config" {
-#  #booted       = false
-#  #comments     = null
-#  kernel       = "linode/grub2"
-#  label        = "My Arch Linux Profile"
-#  linode_id    = linode_instance.site.id
-#  memory_limit = 0
-#  root_device  = "/dev/sda"
-#  run_level    = "default"
-#  virt_mode    = "paravirt"
-#
-#  device {
-#    device_name = "sda"
-#    disk_id     = linode_instance_disk.site_disk.id
-#    volume_id   = 0
-#  }
-#
-#  device {
-#    device_name = "sdb"
-#    disk_id     = linode_instance_disk.site_disk_swap.id
-#    volume_id   = 0
-#  }
-#
-#  helpers {
-#    devtmpfs_automount = true
-#    distro             = true
-#    modules_dep        = true
-#    network            = true
-#    updatedb_disabled  = true
-#  }
-#}
-#
-#resource "linode_instance_disk" "site_disk_swap" {
-#  filesystem = "swap"
-#  label      = "512 MB Swap Image"
-#  linode_id  = linode_instance.site.id
-#  size       = 512
-#}
-#
-#resource "linode_instance_disk" "site_disk" {
-#  filesystem = "ext4"
-#  label      = "Arch Linux Disk"
-#  linode_id  = linode_instance.site.id
-#  size       = 163328
-#}
