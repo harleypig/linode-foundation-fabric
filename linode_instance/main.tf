0d17d1adf331c8e0fe36a44d4fa62d17eaa268a9
Alan Young <harleypig@harleypig.com>
2025-02-10 04:19:50 -0700

    chore: Remove linode_instance resource configuration from main.tf

--- linode_instance/main.tf
+++ linode_instance/main.tf
@@ -1,11 +1,1 @@
-resource "linode_instance" "site" {
-  label           = var.label
-  image           = var.image
-  region          = var.region
-  type            = var.type
-  migration_type  = var.migration_type
-  resize_disk     = var.resize_disk
-  backups_enabled = var.backups
-  tags            = var.tags
-  swap_size       = var.swap_size
-}
+# Create a main.tf for the instance resource, AI!
